59fdd97dc5f93b914511804a26df0c6fc55ae575
VitaliyHreva <vitaliyhreva@gmail.com>
2016-08-08 21:48:27 +0300

    add facebook share button

--- app/scripts/controllers/shop.js
+++ app/scripts/controllers/shop.js
@@ -8,35 +8,43 @@ angular.module('App')
 
     $scope.videos = [
       {
+        cover: 'https://img.youtube.com/vi/K1uj2qeBor4/mqdefault.jpg', 
+        url: 'https://www.youtube.com/embed/K1uj2qeBor4' 
+      },
+
+      {
+        cover: 'https://img.youtube.com/vi/SdmpBeLrzys/mqdefault.jpg', 
+        url: 'https://www.youtube.com/embed/SdmpBeLrzys' 
+      },
+      
+      {
+        cover: 'https://img.youtube.com/vi/Az2IOUPqcVk/mqdefault.jpg', 
+        url: 'https://www.youtube.com/embed/Az2IOUPqcVk'   
+      },
+      
+      {
+        cover: 'https://img.youtube.com/vi/k0vJcYqod7w/mqdefault.jpg', 
+        url: 'https://www.youtube.com/embed/k0vJcYqod7w'   
+      },
+
+      {
+        cover: 'https://img.youtube.com/vi/P9w7aLnAuvQ/mqdefault.jpg', 
+        url: 'https://www.youtube.com/embed/P9w7aLnAuvQ' 
+      },
+
+      {
+        cover: 'https://img.youtube.com/vi/Yrl35yfpnA0/mqdefault.jpg', 
+        url: 'https://www.youtube.com/embed/Yrl35yfpnA0' 
+      },
+
+      {
+        cover: 'https://img.youtube.com/vi/CQzJeBqUW7g/mqdefault.jpg', 
+        url: 'https://www.youtube.com/embed/CQzJeBqUW7g' 
+      },
+
+      {
+        cover: 'https://img.youtube.com/vi/o6cY6BtDK4E/mqdefault.jpg', 
         url: 'https://www.youtube.com/embed/mibLt8rOG8o' 
-      },
-
-      {
-        url: 'https://www.youtube.com/embed/jZfC-MdzA7w'  
-      },
-      
-      {
-        url: 'https://www.youtube.com/embed/mibLt8rOG8o'  
-      },
-      
-      {
-        url: 'https://www.youtube.com/embed/SdmpBeLrzys'  
-      },
-
-      {
-        url: 'https://www.youtube.com/embed/K1uj2qeBor4'
-      },
-
-      {
-        url: 'https://www.youtube.com/embed/yUMcDQlVtQE'
-      },
-
-      {
-        url: 'https://www.youtube.com/embed/o6cY6BtDK4E'
-      },
-
-      {
-        url: 'https://www.youtube.com/embed/SEyDminvr7Q'
       }
     ]
   });
